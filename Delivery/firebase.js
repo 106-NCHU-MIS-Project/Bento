f587b3cd485833365dcf78bb964a2bdf41edd2f9
楊政卿 <godis30300@gmail.com>
2018-04-06 23:06:05 +0800

    0.0

--- Delivery/firebase.js
+++
@@ -1,9 +0,0 @@
-
-var config = {
-  apiKey: "AIzaSyDd2rFHwPXyIkq0iRSinBBfzg31NYJLapE",
-  authDomain: "nchumis2017-5566.firebaseapp.com",
-  databaseURL: "https://nchumis2017-5566.firebaseio.com",
-  projectId: "nchumis2017-5566",
-  storageBucket: "nchumis2017-5566.appspot.com",
-  messagingSenderId: "501160127423"
-};
